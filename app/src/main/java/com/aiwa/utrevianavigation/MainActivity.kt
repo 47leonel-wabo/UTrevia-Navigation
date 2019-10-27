3815ad39175cf3cbc2b69aee8ea34f7c61c4dec6
leonel <waboleonel@yahoo.com>
2019-10-27 19:37:59 +0100

    Intro fragment added

--- app/src/main/java/com/aiwa/utrevianavigation/MainActivity.kt
+++ app/src/main/java/com/aiwa/utrevianavigation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.aiwa.utrevianavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.aiwa.utrevianavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mainBinding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
